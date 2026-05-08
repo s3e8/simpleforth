56056263247443466a3e6360025124611fc1b6aa
s3e8 <sphiax.s3e8@gmail.com>
2026-05-08 00:03:13 +0000

    idk

--- forth.c
+++ forth.c
@@ -54,7 +54,7 @@
 #define STATE_IMMEDIATE 0
 #define STATE_COMPILE   1
 /* the most important type, the cell. MUST be exactly of the pointer length! */
-typedef uintptr_t cell; // todo: use long long or intptr or uintptr?
+typedef intptr_t cell; // todo: use long long or intptr or uintptr?
 /* preprocessor trick to test sizeof(long)==sizeof(void*)? */
 
 /* dictionary definition header. NEVER change the order of these fields, it's crucial! */
